122d8fd6124a76ac55a560313657dc6875157c0c
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-15 17:53:17 +0200

    Made default if null or empty

--- Program.cs
+++ Program.cs
@@ -5,10 +5,18 @@ class Program
     {
         Console.WriteLine("Vad heter du?");
         var name = Console.ReadLine();
-        SayHello(name);
+
+        if (string.IsNullOrEmpty(name))
+        {
+            SayHello();
+        }
+        else
+        {
+            SayHello(name);
+        }
     }
 
-    static void SayHello(string name)
+    static void SayHello(string name = "Gäst")
     {
         Console.WriteLine($"Hej {name}! Välkommen till programmet.");
 
