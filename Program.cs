7c697a727e59f01b361308e341aa7b32341ee8e4
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-15 18:00:10 +0200

    Made clear comments

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask for the user's name
         Console.WriteLine("Vad heter du?");
         var name = Console.ReadLine();
 
+        // Call the method to say hello
         if (string.IsNullOrEmpty(name))
         {
             SayHello();
@@ -18,7 +20,7 @@ class Program
 
     static void SayHello(string name = "Gäst")
     {
+        // Print a greeting message
         Console.WriteLine($"Hej {name}! Välkommen till programmet.");
-
     }
 }
--- obj/Debug/net9.0/SayHello.AssemblyInfo.cs
+++ obj/Debug/net9.0/SayHello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SayHello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5a90d7df1cdf010b593f9652f0d8f7779958ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122d8fd6124a76ac55a560313657dc6875157c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SayHello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SayHello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
