ec5a90d7df1cdf010b593f9652f0d8f7779958ad
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-15 17:39:15 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var name = Console.ReadLine();
+        SayHello(name);
+    }
+
+    static void SayHello(string name)
+    {
+        Console.WriteLine($"Hello {name}! Välkommen till programmet.");
+    }
+}
